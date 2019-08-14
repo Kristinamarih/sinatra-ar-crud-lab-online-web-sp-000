a32a2007a5a47059490faf43773c92652f5a5a77
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 19:01:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do
     @article = Article.create(title: params[:title], content: params[:content])
-    redirect_to 
+    redirect_to "/articles/:id"
   end
   
   get '/articles/:id/edit' do
