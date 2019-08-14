e9af2a10e39d7e2d26bd8a2b12cd42012a884f01
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 18:58:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/articles/:id' do
-    @article = Article.find(params[:id])
+    @article = Article.all.find(params[:id])
     erb :show
   end
   
