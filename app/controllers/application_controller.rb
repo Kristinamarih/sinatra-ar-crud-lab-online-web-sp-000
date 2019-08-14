9f2c8a67048a73e3872b4c099550c48632af5159
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 20:13:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     id = params["id"]
-    @article = Article.all.find(params[:id])
+    @article = Article.find(params[:id])
     @article.update(title: params[:title], content: params[:content])
     @article.save
     redirect to("/articles/#{@article.id}")
