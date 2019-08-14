c678507cbeb7d67e2c63a03cf92c66d854fb9adb
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 20:19:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
     redirect"/articles/#{@article.id}"
   end
   
-  delete '/articles/:id' do
+  delete '/articles/:id/delete' do
     @article = Article.find(params[:id])
     @article.destroy
     erb :show
